2de825a94454f9dc85e17245ecb77c673bb67af7
Mehtari-Nadir <nadirmehtaripr@gmail.com>
2024-07-17 13:42:26 +0100

    Make the h1 in the header a Link

--- app/header.tsx
+++ app/header.tsx
@@ -1,15 +1,16 @@
-import { Brain, Link } from "lucide-react";
+import { Brain} from "lucide-react";
 import { HeaderActions } from "./header-actions";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import Link from "next/link";
 
 export const Header = () => {
     return (
         <div className="bg-gray-900 p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="flex items-center gap-x-1" >
+                <Link href={"/"} className="flex items-center gap-x-1" >
                     <Brain />
                     <h1 className="text-2xl">BigBrain</h1>
-                </div>
+                </Link>
                 <div className="flex items-center gap-2">
                     <ModeToggle />
                     <HeaderActions />
